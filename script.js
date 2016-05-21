df481f263708297ef80a92a31b538f8237b775df
boyboi86 <boyboi86@gmail.com>
2016-05-21 20:08:03 +0800

    final codes

--- script.js
+++ script.js
@@ -1,52 +1,95 @@
-$.get("http://ipinfo.io", function(response) {
-  $("#location").html(response.city);
-}, "jsonp");
+$(document).ready(function() {
+  $.ajax({
+    url: 'http://ip-api.com/json',
+    data: {},
+    dataType: 'json',
+    Method: 'GET',
+    success: function(data) {
+      $city = data.city;
+      $('#location').html($city)
 
-var weatherUrl;
+      setCurrent($city);
+      
+    },
+    error: function(error) {
+      console.log(error)
+    }
+  });
+});
 
-$.get('http://ipinfo.io', function(response) {
+function setCurrent(city) {
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&APPID=f962dceecbf8f2c3a14c48c7aeb9af10',
+    data: {},
+    dataType: 'json',
+    method: 'GET',
+    success: function(data) {
+      $('#temp').empty();
+      measurement = '°C';
+      $('#temp').append(data.main.temp + measurement);
+      $status = data.weather[0].main;
+      $('#icon').empty();
+      setIcon($status);
+      changeDeg(data.main.temp);
+      
+    }
+  })
+}
 
-      weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + response.city + "&units=metric&APPID=f962dceecbf8f2c3a14c48c7aeb9af10";
-      $.getJSON(weatherUrl, function(json) {
-            $("#temp").html(json.main.temp_min + '&#176' + 'C');
+function setIcon(status) {
 
-            $.ajax(weatherURL,
-                   data:{},
-                   dataType:'json',
-                   method:'GET'
-                   success: function(data){
-                   function getIcon() { 
-                switch (status) {
-                  case 'Wind':
-                    $('#icon').append('<i class="wi wi-cloudy-windy"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/clear.jpg')
-                    break;
-                  case 'Cloud':
-                    $('#icon').append('<i class="wi wi-cloudy"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/cloud.jpg')
-                    break;
-                  case 'Rain':
-                    $('#icon').append('<i class="wi wi-rain-mix"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/rain.jpg')
-                    break;
-                  case 'Snow':
-                    $('#icon').append('<i class="wi wi-snow"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/snow.jpg')
-                    break;
-                  case 'Fog':
-                    $('#icon').append('<i class="wi wi-fog"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/fog.jpg')
-                    break;
-                  case 'Haze':
-                    $('#icon').append('<i class="wi wi-rain-mix"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/haze.jpg')
-                    break;
-                  case 'Drizzle':
-                    $('#icon').append('<i class="wi wi-sprinkle"></i>');
-                    $('body').css('background-image', 'weather_forecast_slick/asset/img/drizzle.jpg')
+  switch (status) {
+    case 'Rain':
+      $('#icon').html('<i class="wi wi-rain-mix"></i>');
+      $('body').css('background-image', 'url("asset/img/drizzle.jpg")');
+      break;
+    case 'Drizzle':
+      $('#icon').html('<i class="wi wi-rain-mix"></i>');
+      $('body').css('background-image', 'url("asset/img/drizzle.jpg")');
+      break;
+    case 'Clear':
+      $('#icon').html('<i class="wi wi-day-sunny"></i>');
+      $('body').css('background-image', 'url("asset/img/clear.jpg")')
+      break;
+    case 'Clouds':
+      $('#icon').html('<i class="wi wi-cloudy"></i>');
+      $('body').css('background-image', 'url("asset/img/cloud.jpg")')
+      break;
+    case 'Thunderstorm':
+      $('#icon').html('<i class="wi wi-storm-showers"></i>');
+      $('body').css('background-image', 'url("asset/img/rain.jpg")')
+      break;
+    case 'Snow':
+      $('#icon').html('<i class="wi wi-snow"></i>');
+      $('body').css('background-image', 'url("asset/img/snow.jpg")')
+      break;
+    case 'Mist':
+      $('#icon').html('<i class="wi wi-fog"></i>');
+      $('body').css('background-image', 'url("asset/img/fog.jpg")')
+      break;
+    case 'Fog':
+      $('#icon').html('<i class="wi wi-fog"></i>');
+      $('body').css('background-image', 'url("asset/img/fog.jpg")')
+      break;
+    case 'Haze':
+      $('#icon').html('<i class="wi wi-smoke"></i>');
+      $('body').css('background-image', 'url("asset/img/fog.jpg")')
+      break;
+    default:$('body').html('Something went wrong please go to github')
+  }
+}
 
-                }
-              }
-            }
+//I tried toggleClass, toggle, even plain vanilla JS
+//the only way is to refresh the page which is counter-intuitive to user
+//tried switch and ternary expression which bear the same result as below
+//major screw ups all over god-damm it F**K this Sh*t
 
-                   
+function changeDeg(value) {
+  $('#switch').click(function() {
+    if ($('#temp').text().indexOf('C') > -1){
+  return $('#temp').text(Math.round((value) * 1.8000 + 32.00) + '°F')
+    } else {
+  return $('#temp').text(Math.round(value) + '°C');
+    } 
+  })
+}
